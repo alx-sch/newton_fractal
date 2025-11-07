e293e794805ea02f84ed8bb593278dba62bda24d
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2025-11-07 16:33:12 +0000

    Refactor: Sort constants defining fractal appearance to top and comment them for user

--- include/defines.hpp
+++ include/defines.hpp
@@ -1,21 +1,7 @@
 #ifndef DEFINES_HPP
 # define DEFINES_HPP
 
-# include "fractal_ispc.h" // for Complex struct from ISPC
-using Complex = ispc::Complex;
-
-# define OUTPUT_DIR	"out"	// Directory to save output files
-
-# define DEF_WIDTH	800	// Default image width
-# define DEF_HEIGHT	800	// Default image height
-
-# define MAX_ITERS	100	// Max iterations for Newton's method
-
-// Adjust brightness of colors
-// - 0.0: no contrast / brighter,
-// - 1.0: original/linear
-// - >1.0: more contrast / darker
-constexpr double GAMMA =	8.0;
+// ### ADJUST THESE TO CHANGE IMAGE PARAMETERS ###
 
 // The viewport defines the area of the complex plane to visualize
 // x is the real part, y is the imaginary part
@@ -24,8 +10,36 @@ constexpr double DEF_VIEW_MAX_X =	2.0;	// Default viewport maximum x
 constexpr double DEF_VIEW_MIN_Y =	-2.0;	// Default viewport minimum y
 constexpr double DEF_VIEW_MAX_Y =	2.0;	// Default viewport maximum y
 
+// Max iterations for Newton's method
+// Higher values yield more detail around the edges (color instead of black)
+// but increase computation time
+# define MAX_ITERS					100
+
+// Adjust brightness of colors
+// - 0.0: no contrast / brighter,
+// - 1.0: original/linear
+// - >1.0: more contrast / darker
+constexpr double GAMMA =			8.0;
+
+// Tolerance for convergence checks:
+// Distance to root smaller than this is considered converged.
+// Use larger values for faster rendering with less detail
+// especially close to or bigger than 1.0, this gets funky!
+constexpr double DEF_TOLERANCE =	1e-6;
+
+// ################################################
+// ################################################
+
+// Use the ISPC struct for Complex numbers
+# include "fractal_ispc.h"
+using Complex = ispc::Complex;
+
+# define OUTPUT_DIR	"out"	// Directory to save output files
+
+# define DEF_WIDTH	800	// Default image width
+# define DEF_HEIGHT	800	// Default image height
+
 constexpr double EPSILON =			1e-10; // Check if floating-point number is zero
-constexpr double DEF_TOLERANCE =	1e-6;  // Tolerance for convergence checks
 
 # define YELLOW		"\033[33m"
 # define RED		"\033[31m"
@@ -42,14 +56,6 @@ constexpr double DEF_TOLERANCE =	1e-6;  // Tolerance for convergence checks
 # else
 #  define DEBUG_PRINT(x) do {} while (0)
 # endif
-
-// // Struct to hold a complex number, shared between C++ host and ISPC kernel
-// // Share `roots` and `z` arrays.
-// struct	Complex
-// {
-// 	double	real;
-// 	double	imag;
-// };
 
 // Structure to hold RGB color values
 struct Color
